e2b5d80d51303b67913adbc499f026e9a3b32af9
Venomlord123 <67080748+Venomlord123@users.noreply.github.com>
2021-06-22 16:57:33 +1000

    Player Prefab created also started player movement properly

--- Assets/TeamMembers/Zach/PlayerMovement.cs
+++ Assets/TeamMembers/Zach/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody rigidBody;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidBody.AddRelativeForce(new Vector3(100,0,0));
+        }
     }
 }
