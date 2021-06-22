a6acea4e6dc5b711be59a8258f0da472d6e02111
Venomlord123 <1019559@student.sae.edu.au>
2021-06-22 12:26:15 +1000

    Editor Scripts

---
+++ Assets/Editor/EditorScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GameManager))]
+public class EditorScript : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        DrawDefaultInspector();
+
+        GameManager gameManager = (GameManager) target;
+        
+        if (GUILayout.Button("Death Test"))
+        {
+            gameManager.Death();
+        }
+    }
+
+}
