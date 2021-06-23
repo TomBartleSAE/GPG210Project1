c38612221b1b55bffe11319fd4540a8e543acf22
Venomlord123 <67080748+Venomlord123@users.noreply.github.com>
2021-06-23 13:41:46 +1000

    More Player code

--- Assets/TeamMembers/Zach/PlayerMovement.cs
+++ Assets/TeamMembers/Zach/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -15,9 +16,24 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Keyboard.current.wKey.isPressed)
         {
             rigidBody.AddRelativeForce(new Vector3(100,0,0));
+        }
+
+        if (Keyboard.current.sKey.isPressed)
+        {
+            rigidBody.AddRelativeForce(new Vector3(-100,0,0));
+        }
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            rigidBody.AddRelativeTorque(new Vector3(0,-5,0));
+        }
+
+        if (Keyboard.current.dKey.isPressed)
+        {
+            rigidBody.AddRelativeTorque(new Vector3(0,5,0));
         }
     }
 }
