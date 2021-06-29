5d1af5626d0def16c9dcc1b3c50f04f58e945e11
Venomlord123 <67080748+Venomlord123@users.noreply.github.com>
2021-06-29 16:51:49 +1000

    Updates to Player Movement again, import of new player model

--- Assets/TeamMembers/Zach/PlayerMovement.cs
+++ Assets/TeamMembers/Zach/PlayerMovement.cs
@@ -8,44 +8,40 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rigidBody;
     private ZachsPlayerActions zachsPlayerActions;
+    public Vector3 velocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        /*zachsPlayerActions = new ZachsPlayerActions();
+        zachsPlayerActions = new ZachsPlayerActions();
         zachsPlayerActions.Enable();
         
-        zachsPlayerActions.Main.Move.performed += Movement;
-        zachsPlayerActions.Main.Rotate.performed += RotateOnPerformed;*/
+        zachsPlayerActions.Main.Move.started += Movement;
+        //zachsPlayerActions.Main.Move.performed += ResettingMovement;
+        zachsPlayerActions.Main.Rotate.started += RotateOnPerformed;
     }
 
-    public void Update()
+   
+
+    public void FixedUpdate()
     {
-        if (Keyboard.current.wKey.isPressed)
-        {
-            rigidBody.AddRelativeForce(new Vector3(40,0,0));
-        }
-        if (Keyboard.current.sKey.isPressed)
-        {
-            rigidBody.AddRelativeForce(new Vector3(-40,0,0));
-        }
-        if (Keyboard.current.aKey.isPressed)
-        {
-            transform.Rotate(new Vector3(0,-5,0));
-        }
-        if (Keyboard.current.dKey.isPressed)
-        {
-            transform.Rotate(new Vector3(0,5,0));
-        }
+        rigidBody.velocity = velocity;
     }
 
 
     void Movement(InputAction.CallbackContext obj)
     {
-        Debug.Log("Moving");
-        rigidBody.AddRelativeForce(new Vector3(100 * obj.ReadValue<float>(),0,0));
+        
+        velocity = new Vector3(100 * obj.ReadValue<float>(), 0, 0);
+        if (obj.phase == InputActionPhase.Canceled)
+        {
+            Debug.Log("Ending Movement");
+        }
     }
-
+    private void ResettingMovement(InputAction.CallbackContext obj)
+    {
+        velocity = new Vector3(0, 0, 0);
+    }
     private void RotateOnPerformed(InputAction.CallbackContext obj)
     {
         //rigidBody.AddRelativeTorque(new Vector3(0,5*obj.ReadValue<float>(),0));
