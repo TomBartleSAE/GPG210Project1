0923d184edef68c8ad3e849f6af2fede5836a813
TomBartleSAE <1022049@student.sae.edu.au>
2021-06-30 11:38:56 +1000

    Hotfix for moving obstacles

--- Assets/TeamMembers/Tom/Scripts/MovingObstacle.cs
+++ Assets/TeamMembers/Tom/Scripts/MovingObstacle.cs
@@ -9,10 +9,17 @@ namespace Tom
     public class MovingObstacle : NetworkBehaviour
     {
         public float spawnForce; // Force applied when spawned
-
+        
+        /*
         public override void OnStartServer()
         {
             base.OnStartServer();
+            GetComponent<Rigidbody>().AddRelativeForce(transform.forward * spawnForce); // Push object forward when spawned
+        }
+        */
+
+        private void Start()
+        {
             GetComponent<Rigidbody>().AddRelativeForce(transform.forward * spawnForce); // Push object forward when spawned
         }
     }
