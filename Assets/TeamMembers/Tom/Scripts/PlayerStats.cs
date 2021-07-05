1cfbaabc8c23c1f0dc151bc21fd81b134f5cdad5
TomBartleSAE <1022049@student.sae.edu.au>
2021-07-05 21:11:13 +1000

    Added scoreboard UI prefab

--- Assets/TeamMembers/Tom/Scripts/PlayerStats.cs
+++ Assets/TeamMembers/Tom/Scripts/PlayerStats.cs
@@ -7,10 +7,21 @@ namespace Tom
 {
     public class PlayerStats : MonoBehaviour
     {
-        public Text nameText, livesText, socreText;
-        public string nameValue;
-        public int livesValue, scoreValue;
-        
-        
+        public Text nameText, livesText, scoreText;
+
+        public void SetName(string newName)
+        {
+            nameText.text = newName;
+        }
+
+        public void SetLives(int newLives)
+        {
+            livesText.text = newLives.ToString();
+        }
+
+        public void SetScore(int newScore)
+        {
+            nameText.text = newScore.ToString();
+        }
     }
 }
--- Assets/TeamMembers/Tom/Scripts/Scoreboard.cs
+++ Assets/TeamMembers/Tom/Scripts/Scoreboard.cs
@@ -6,12 +6,15 @@ namespace Tom
 {
     public class Scoreboard : MonoBehaviour
     {
-        public GameObject newPlayerStats;
-        public Transform contentHolder;
+        public PlayerStats newPlayerStats;
         
         public void AddPlayer()
         {
-            Instantiate(newPlayerStats, contentHolder);
+            PlayerStats newPlayer = Instantiate(newPlayerStats, transform);
+            
+            // Consider getting player number from a list of all player objects rather than child count
+            // Could also assign a colour to each player and pass the name of that colour here?
+            newPlayer.SetName("Player " + (transform.childCount - 1));
         }
     }
 }
