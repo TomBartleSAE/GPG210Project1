09ab5c929d62e0ba3504b460717126bb64cfd369
Tom Bartle <1022049@student.sae.edu.au>
2021-07-07 12:34:01 +1000

    Added main menu with tweens

--- Assets/TeamMembers/Tom/Scripts/MainMenu.cs
+++ Assets/TeamMembers/Tom/Scripts/MainMenu.cs
@@ -1,17 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Tom
 {
     public class MainMenu : MonoBehaviour
     {
-        public Transform menu;
+        public MovingTween background;
+        public CanvasAlphaTween title;
+        public CanvasAlphaTween buttons;
+
+        private bool menuShowing = false;
         
-        private void Toggle()
+        private void Start()
         {
-            
+            ShowMenu();
+        }
+
+        private void Update()
+        {
+            // HACK
+            // Change to new input system
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (menuShowing)
+                {
+                    HideMenu();
+                }
+                else
+                {
+                    ShowMenu();
+                }
+            }
+        }
+
+        public void ShowMenu()
+        {
+            Sequence sequence = DOTween.Sequence();
+
+            sequence.Append(background.Move(1000, 0, 2).SetEase(Ease.OutBounce));
+            sequence.Append(title.Fade(0, 1, 1));
+            sequence.Append(buttons.Fade(0, 1, 1));
+
+            menuShowing = true;
+        }
+
+        public void HideMenu()
+        {
+            Sequence sequence = DOTween.Sequence();
+
+            sequence.Append(background.Move(0, 1000, 2).SetEase(Ease.InBounce));
+
+            menuShowing = false;
         }
     }
 }
