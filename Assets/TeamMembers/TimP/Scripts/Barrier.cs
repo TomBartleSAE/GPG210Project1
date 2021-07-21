400051c214de0b53fac58b28b772ac15109087a0
Tom Bartle <1022049@student.sae.edu.au>
2021-07-21 10:40:59 +1000

    Updated barrier to prevent flickering

--- Assets/TeamMembers/TimP/Scripts/Barrier.cs
+++ Assets/TeamMembers/TimP/Scripts/Barrier.cs
@@ -8,34 +8,16 @@ public class Barrier : NetworkBehaviour
 {
     public GameObject pairedBarrier;
     public float offset = 1f;
-    
-    public bool xTele;
-    public bool zTele;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // if xTele = true, then teleport code
-        if (xTele == true)
-        {
-            //teleport along x axis
-        }
-
-        if (zTele == true)
-        {
-            //teleport along z axis
-        }
-    }
 
     private void OnTriggerEnter(Collider other)
     {
-        // Offset allows for object to teleport in front of other barrier instead of inside it
-        float distance = Vector3.Distance(transform.position, pairedBarrier.transform.position) - offset;
-        other.transform.position += transform.forward * distance;
+        float dotProduct = Vector3.Dot(other.GetComponent<Rigidbody>().velocity.normalized, transform.forward);
+
+        if (dotProduct < 0)
+        {
+            // Offset allows for object to teleport in front of other barrier instead of inside it
+            float distance = Vector3.Distance(transform.position, pairedBarrier.transform.position) - offset;
+            other.transform.position += transform.forward * distance;
+        }
     }
 }
