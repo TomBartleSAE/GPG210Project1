e75251a2a2bf9939ddfd565ad00406c3083f7e74
Tom Bartle <1022049@student.sae.edu.au>
2021-08-09 17:00:24 +1000

    Updated barrier

--- Assets/TeamMembers/TimP/Scripts/Barrier.cs
+++ Assets/TeamMembers/TimP/Scripts/Barrier.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 public class Barrier : NetworkBehaviour
 {
     public GameObject pairedBarrier;
-    public float offset = 1f;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,8 +15,7 @@ public class Barrier : NetworkBehaviour
         if (dotProduct < 0)
         {
             // Offset allows for object to teleport in front of other barrier instead of inside it
-            float distance = Vector3.Distance(transform.position, pairedBarrier.transform.position) - offset;
-            other.transform.position += transform.forward * distance;
+            other.transform.position = pairedBarrier.transform.position;
         }
     }
 }
