db9feaafd633c31b69d420e59ed11024087441c4
tim pearson <1018360@student.sae.edu.au>
2021-08-09 17:14:20 +1000

    testing changes to spawner to make it respawn

--- Assets/TeamMembers/TimP/Scripts/Spawner.cs
+++ Assets/TeamMembers/TimP/Scripts/Spawner.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
 using Tom;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Tim
 {
@@ -14,10 +16,12 @@ public class Spawner : NetworkBehaviour
     private Vector3 cPos;
 
     public SpawnManager spawnManager;
+    public bool startSpawn = false;
 
     public override void OnStartServer()
     {
         base.OnStartServer();
+        startSpawn = true;
         spawnManager.SpawnObstacles += SpawnOnServerStart;
         cPos = new Vector3(transform.position.x+Random.Range(0,10), .5f, transform.position.z+Random.Range(0,10));
         //this.asteroidSpawn(asteroid,cPos);
@@ -25,13 +29,36 @@ public class Spawner : NetworkBehaviour
 
     private void SpawnOnServerStart()
     {
-        this.asteroidSpawn(asteroid,cPos);
+        //this.asteroidSpawn(asteroid,cPos);
+        if (startSpawn == true)
+        {
+            StartCoroutine(asteroidSpawn(asteroid, cPos));
+        }
     }
 
-    public void asteroidSpawn(GameObject aGame,Vector3 aSpawn)
-    {
+    /*public void asteroidSpawn(GameObject aGame,Vector3 aSpawn)
+      {
         GameObject asteroidSpawn = Instantiate(aGame, aSpawn, Quaternion.identity);
         NetworkServer.Spawn(asteroidSpawn);
+      }*/
+
+    private void FixedUpdate()
+    {
+        if (startSpawn == true)
+        {
+            StartCoroutine(asteroidSpawn(asteroid, cPos));
+        }
+        
+    }
+
+    public IEnumerator asteroidSpawn(GameObject aGame, Vector3 aSpawn)
+    {
+        startSpawn = false;
+        GameObject asteroidSpawn = Instantiate(aGame, aSpawn, Quaternion.identity);
+        NetworkServer.Spawn(asteroidSpawn);
+        Debug.Log("test spawn");
+        yield return new WaitForSeconds(20f);
+        startSpawn = true;
     }
 }
 }
